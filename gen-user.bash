c00c71779cb702ad22b58fd1fd5241a2d9d6d7b4
Robert Elia <relia@pdx.edu>
2025-04-25 23:08:31 -0700

    Updated script for inserting a new user

--- gen-user.bash
+++ gen-user.bash
@@ -2,42 +2,47 @@
 
 # Displays how to use the script
 usage() {
-  echo "Usage: $0 <username> <role>"
-  echo "Creates a new user with specified role"
-  echo "Valid roles are 'user', 'facilitator', or 'admin'"
+    echo -e "Usage: $0 <first_name> <last_name> <email> <role>\n"
+    echo "Creates a new user with specified role"
+    echo -e "Valid roles are 'user', 'facilitator', or 'admin'\n"
+    exit 1
 }
 # Check if database container is running
 check_db_container() {
-  if [ -z "$(docker ps -q -f name=unconfrs-db-1)" ]; then
-    echo "Database container is not running"
-    exit 1
-  fi
+    if [ -z "$(docker ps -q -f name=unconfrs-db-1)" ]; then
+        echo "Database container is not running"
+        exit 1
+    fi
 }
 
 # Update password in database
 create_user() {
-  local username="$1"
-  local role="$2"
-  local password="$3"
+    local fname="$1"
+    local lname="$2"
+    local email="$3"
+    local role="$4"
+    local password="$5"
 
-  check_db_container
+    check_db_container
 
-  sql="INSERT INTO users (username, password) VALUES ('$username', '$password')"
-  docker exec -i unconfrs-db-1 psql -d db -c "$sql"
-  sql="INSERT INTO users_groups (user_id, group_id) VALUES ((SELECT id FROM users WHERE username = '$username'), (SELECT id FROM groups WHERE name = '$role'))"
-  docker exec -i unconfrs-db-1 psql -d db -c "$sql"
+    sql="INSERT INTO users (fname, lname, email, password) VALUES ('$fname', '$lname', '$email', '$password')"
+    docker exec -i unconfrs-db-1 psql -d db -c "$sql"
+    sql="INSERT INTO users_groups (user_id, group_id) VALUES ((SELECT id FROM users WHERE email = '$email'), (SELECT id FROM groups WHERE name = '$role'))"
+    docker exec -i unconfrs-db-1 psql -d db -c "$sql"
 }
 
-# Make sure username was provided
-if [ $# -ne 2 ]; then
-  usage
+# Make sure correct number of args were provided
+if [ $# -ne 4 ]; then
+    usage
 fi
 
-username="$1"
-role="$2"
+fname="$1"
+lname="$2"
+email="$3"
+role="$4"
 
 if [ "$role" != "user" ] && [ "$role" != "facilitator" ] && [ "$role" != "admin" ]; then
-  usage
+    usage
 fi
 
 # Use htpasswd to generate bcrypt hash
@@ -45,4 +50,4 @@ fi
 # Default version is 2b
 hashed_pw=$(htpasswd -nBC 12 "" | tr -d ':')
 
-create_user "$username" "$role" "$hashed_pw"
+create_user "$fname" "$lname" "$email" "$role" "$hashed_pw"
