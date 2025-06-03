23d65c7de9a62a7b8d8c80ab2e0900be6d22d3f5
Robert Elia <relia@pdx.edu>
2025-06-02 19:41:15 -0700

    Update script to only update passwords and to use email not username

--- update-password.bash
+++ update-password.bash
@@ -2,9 +2,10 @@
 
 # Displays how to use the script
 usage() {
-  echo "Usage: $0 <username>"
-  echo "Updates or sets the password for the specified username in the database"
+  echo "Usage: $0 <email>"
+  echo "Updates the password for the specified email in the database"
 }
+
 # Check if database container is running
 check_db_container() {
   if [ -z "$(docker ps -q -f name=unconfrs-db-1)" ]; then
@@ -15,26 +16,25 @@ check_db_container() {
 
 # Update password in database
 update_password() {
-  local username="$1"
+  local email="$1"
   local password="$2"
 
   check_db_container
 
-  sql="INSERT into users (username, password) VALUES ('$username', '$password')
-  ON CONFLICT (username) DO UPDATE SET password = '$1'"
+  sql="UPDATE users SET password = '$password' WHERE email = '$email'"
   docker exec -i unconfrs-db-1 psql -d db -c "$sql"
 }
 
-# Make sure username was provided
+# Make sure email was provided
 if [ $# -ne 1 ]; then
   usage
 fi
 
-username="$1"
+email="$1"
 
 # Use htpasswd to generate bcrypt hash
 # Default cost is 12
 # Default version is 2b
 hashed_pw=$(htpasswd -nBC 12 "" | tr -d ':')
 
-update_password "$username" "$hashed_pw"
+update_password "$email" "$hashed_pw"
